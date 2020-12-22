af46fcc2f677696fc0e86334ab7a67d51f97ed79
hannah-developer <hannahchoi.developer@gmail.com>
2020-12-22 21:55:41 +0000

    added setDisplayvalue & setSavedValue

--- src/components/NumberProvider.js
+++ src/components/NumberProvider.js
@@ -3,7 +3,26 @@ import React, { useState } from "react";
 export const NumberContext = React.createContext();
 
 function NumberProvider(props) {
-    return <NumberContext.provider></NumberContext.provider>;
+    const [number, setNumber] = useState("");
+    const [savedNumber, setSavedNumber] = useState("");
+    const [operator, setOperator] = useState("");
+
+    const setDisplayValue = num => {
+        if ((!number.includes(".") || num !== ".") && number.length < 8) {
+            setNumber(`${(number + num).replace(/^0+/, "")}`);
+        }
+    };
+
+    const setSavedValue = () => {
+        setSavedNumber(number);
+        setNumber("");
+    };
+
+    return (
+        <NumberContext.provider value={{ setSavedValue, setDisplayValue }}>
+            {props.children}
+        </NumberContext.provider>
+    );
 }
 
 export default NumberProvider;
